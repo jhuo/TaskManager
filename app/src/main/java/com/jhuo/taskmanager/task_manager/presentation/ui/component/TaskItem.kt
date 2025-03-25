194d63d075a153872c78f42f50b579eb632d69d2
Jerry Huo <jerry08huo@yahoo.com>
2025-03-25 01:16:28 -0500

    ui update

--- app/src/main/java/com/jhuo/taskmanager/task_manager/presentation/ui/component/TaskItem.kt
+++ app/src/main/java/com/jhuo/taskmanager/task_manager/presentation/ui/component/TaskItem.kt
@@ -1,6 +1,7 @@
 package com.jhuo.taskmanager.task_manager.presentation.ui.component
 
 import StatusSelector
+import android.R.attr.text
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
@@ -117,15 +119,16 @@ private fun ExpandedTaskContent(
 
 @Composable
 fun DateItems(label: String, value: String) {
-    Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
-        Text(text = label, fontSize = 14.sp, modifier = Modifier.weight(0.5f), style = MaterialTheme.typography.labelSmall)
-        Text(text = value, fontSize = 14.sp, style = MaterialTheme.typography.labelSmall)
+    Row(modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp), verticalAlignment = Alignment.CenterVertically) {
+        Text(text = label, style = MaterialTheme.typography.labelSmall)
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(text = value, style = MaterialTheme.typography.labelSmall)
     }
 }
 
 @Composable
 fun StatusIcon(status: TaskStatus) {
-    Box(modifier = Modifier.padding(end = 4.dp)) {
+    Box(modifier = Modifier.padding(end = 8.dp)) {
         Icon(
             painter = painterResource(
                 id = when (status) {
